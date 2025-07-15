b345ec545a32e3ee2474e7fcc11d590ae5a2db6a
yurika-s <200558167@student.georgianc.on.ca>
2025-07-15 17:33:48 -0400

    Added ImageGallery component to App.js and adjusted App.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import ImageGallery from './components/ImageGallery';
 
 function App() {
+  const [imageData, setImageData] = useState([]);
+
+  const url = 'https://dog.ceo/api/breeds/image/random/12';
+
+  useEffect(() => {
+    // set initial dog images
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setImageData(data.message);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Dog Gallery</h2>
+      <div className='APP-container'>
+        <ImageGallery itemData={imageData} />
+      </div>
     </div>
   );
 }
