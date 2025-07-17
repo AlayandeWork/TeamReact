3ded9523a45ae9d35750eae3a812d7da02ff33f3
AbdulMatin Alayande Nuhu <Alayandework@gmail.com>
2025-07-17 10:02:42 -0400

    added the API KEY for weather and time zone

--- src/App.js
+++ src/App.js
@@ -2,3 +2,6 @@ import React, { useEffect, useRef, useState } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import './App.css';
+
+const API_KEY = '36be86306e394f9b948a73620e0e4845';
+const TIMEZONE_API_KEY = 'AC3QUERYD3GQ';
